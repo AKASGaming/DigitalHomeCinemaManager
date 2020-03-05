779070e4d43f5239e9987451150e367267d96a1b
Bill Mandra <wmandra@mac.com>
2020-03-04 21:26:11 -0500

    start on LightingController abstract class

---
+++ DigitalHomeCinemaControl/Controllers/Base/LightingController.cs
@@ -0,0 +1,54 @@
+﻿/*
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ */
+
+namespace DigitalHomeCinemaControl.Controllers.Base
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using DigitalHomeCinemaControl.Components.Lighting;
+
+    public abstract class LightingController : DeviceController, ILightingController
+    {
+
+        public LightingController()
+            : base()
+        {
+            
+        }
+
+        public string RouteAction(string action, object args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetScene(string scence, bool state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Index { get; set; }
+
+        public Dictionary<string, LightingScene> Scenes { get; protected set; }
+
+        public string Name { get; protected set; }
+
+        public IDictionary<string, Type> Actions { get; protected set; }
+
+        
+    }
+
+}
